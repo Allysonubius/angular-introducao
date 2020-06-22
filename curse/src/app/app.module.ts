f7a81cecf95a2cfe225bf01c7a5e2a9e3c6b989b
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2020-06-22 12:11:15 -0300

    Realizado criação dos componentes e efetuado construção para o projeto.

--- curse/src/app/app.module.ts
+++ curse/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule}  from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CourseListComponent} from './courses/course-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModulea,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations:[
+		AppComponent,
+		CourseListComponent
+	],
+	imports:[
+		BrowserModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule{}
