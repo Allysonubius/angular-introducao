ff85c81e8a15f23c3cd4423e31a0221701166eaa
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2020-06-22 16:05:40 -0300

    Protegendo as rotas com segurança dentro do Angular.

---
+++ curse/src/app/courses/ERROR 404/ERROR-404.component.ts
@@ -0,0 +1,8 @@
+import (Component) from '@angular/core';
+
+@Component({
+	templateUrl: ' ./ERROR-404.component.html'
+})
+export class ERROR404Component{
+	
+}
--- curse/src/app/courses/course-list.component.ts
+++ curse/src/app/courses/course-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Course} from './course';
 
 @Component({
-    selector:'app-course-list',
     template: '<h2> Course List </h2>'
 })
 export class CourseListComponent implements OnInit{
