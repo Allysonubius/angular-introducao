405c0124da92601b5d7cb5ad2edf8b1a84a1f6f1
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2020-06-21 21:56:45 -0300

    Realizado trabalho com moduloes e componentes.

--- curse/src/app/app.component.ts
+++ curse/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curse';
+  title = 'course-manager';
+
+  name:string = 'Allyson';
 }
